47abbb7df53abf8c6223c1877d724dae6a22a464
Keiichiro Ui <keiichiro.ui@gmail.com>
2014-02-28 00:25:38 +0900

    fix installation bug

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ BASE="$(cd "$(dirname $0)"; pwd)"
 
 cd "$BASE"
 
+mkdir -vp $HOME/.local/bin $HOME/.config/autostart
+
 install --mode=755 --verbose tccs $HOME/.local/bin
 install --mode=755 --verbose tccs.desktop $HOME/.config/autostart
